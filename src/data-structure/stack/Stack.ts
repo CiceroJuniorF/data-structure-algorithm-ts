7511796ef082f1d50aecb926052cceec505704a1
Cicero Junior <cicero.junior@zabit.com.br>
2022-01-22 10:02:26 -0300

    refactor: ident

--- src/data-structure/stack/Stack.ts
+++ src/data-structure/stack/Stack.ts
@@ -1,36 +1,34 @@
 export default class Stack<T> {
     private _items: T[];
 
-    constructor(){
+    constructor() {
         this._items = []
     }
 
-    public push(item:T): void{
+    public push(item: T): void {
         this._items.push(item);
     }
 
-    public size(): number{
+    public size(): number {
         return this._items.length;
     }
 
-    public isEmpty(): boolean{
+    public isEmpty(): boolean {
         return this.size() == 0;
     }
 
-    public clear(): void{
-         this._items = [];
+    public clear(): void {
+        this._items = [];
     }
 
-    public peek(): T{
-        if(this.isEmpty()) throw "Stack is Empty";
+    public peek(): T {
+        if (this.isEmpty()) throw "Stack is Empty";
         return this._items.slice(-1)[0];
-   }
+    }
 
-   public pop(): T{
-    if(this.isEmpty()) throw "Stack is Empty";
-    return this._items.pop();
-}
-
-    
+    public pop(): T {
+        if (this.isEmpty()) throw "Stack is Empty";
+        return this._items.pop();
+    }
 
 }
